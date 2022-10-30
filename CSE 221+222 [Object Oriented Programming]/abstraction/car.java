1233145be98e310e4640b8c8139d2a9fefa9b1b8
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2022-10-30 13:18:16 +0600

    Update car.java

--- CSE 221+222 [Object Oriented Programming]/abstraction/car.java
+++ CSE 221+222 [Object Oriented Programming]/abstraction/car.java
@@ -1,7 +1,8 @@
+// 100% abstraction
 public class car extends vehicle {
     public void move(){
         System.out.println("Car moves faster");
-    }
+    }                                                   
     public static void main(String[]args){
         car c1=new car();
         c1.move();
