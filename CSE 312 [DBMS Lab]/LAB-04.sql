8cafe91e7b9a0cab7f6fa9b2b4812d03cffee425
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2023-08-24 11:26:14 +0600

    Update LAB-04.sql

--- CSE 312 [DBMS Lab]/LAB-04.sql
+++ CSE 312 [DBMS Lab]/LAB-04.sql
@@ -5,7 +5,7 @@ SELECT (50+60) as Addition,
  (50%60) as Modulous;
 
 
-update products set price = price+20 where ProductName = 'Tofu'
+update products set price = price+20 where ProductName = 'Tofu';
 
 SELECT ProductName as L20 FROM products WHERE price < 20;
 SELECT ProductName as G20 FROM products WHERE price > 20;
@@ -13,4 +13,6 @@ SELECT ProductName as LE50 FROM products WHERE price <= 50;
 SELECT ProductName as GE50 FROM products WHERE price >= 50;
 SELECT ProductName as NE50 FROM products WHERE Price <> 50;
 
-SELECT CustomerName FROM customers WHERE Country = 'Germany' OR City = 'London'
+SELECT CustomerName FROM customers WHERE Country = 'Germany' OR City = 'London';
+
+SELECT CustomerName FROM customers WHERE Country = 'Germany' ANd CustomerID>=5 AND CustomerID<=10;
