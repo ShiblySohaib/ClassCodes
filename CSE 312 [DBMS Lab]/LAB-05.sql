016fb31465444908ebd021e53538117ba49d621e
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2023-09-14 13:47:56 +0600

    Update LAB-05.sql

--- CSE 312 [DBMS Lab]/LAB-05.sql
+++ CSE 312 [DBMS Lab]/LAB-05.sql
@@ -1,9 +1,9 @@
-SELECT * FROM employee_table ORDER BY NAME;
-SELECT * FROM employee_table ORDER BY COUNTRY DESC;
-SELECT * FROM employee_table ORDER BY COUNTRY DESC, AGE ASC;
-SELECT COUNTRY, COUNT(*) FROM employee_table GROUP BY COUNTRY; 
-SELECT COUNTRY, COUNT(*) FROM employee_table GROUP BY COUNTRY HAVING COUNT(*)>1;
-SELECT COUNTRY, MAX(AGE) FROM employee_table GROUP BY COUNTRY HAVING MAX(AGE)<50 ORDER BY COUNTRY DESC;
+SELECT * FROM employee_table ORDER BY NAME; /*sort*/
+SELECT * FROM employee_table ORDER BY COUNTRY DESC; /*desc order*/
+SELECT * FROM employee_table ORDER BY COUNTRY DESC, AGE ASC; /*first sort country desc, then sort age asc*/
+SELECT COUNTRY, COUNT(*) FROM employee_table GROUP BY COUNTRY; /*group by country, count of each country*/
+SELECT COUNTRY, COUNT(*) FROM employee_table GROUP BY COUNTRY HAVING COUNT(*)>1; /*group by country, show those having count>1*/
+SELECT COUNTRY, MAX(AGE) FROM employee_table GROUP BY COUNTRY HAVING MAX(AGE)<50 ORDER BY COUNTRY DESC; 
 SELECT COUNTRY, MAX(AGE) FROM employee_table GROUP BY COUNTRY HAVING MAX(AGE)<50 ORDER BY COUNTRY DESC LIMIT 2;
 
 
