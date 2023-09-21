eff577fc43f5d4f222d1e0b57b8b3e7c5b0ebac3
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2023-09-21 12:54:48 +0600

    Update LAB-06.sql

--- CSE 312 [DBMS Lab]/LAB-06.sql
+++ CSE 312 [DBMS Lab]/LAB-06.sql
@@ -1,2 +1,6 @@
 /*Inner join*/
 SELECT customers.id, customers.name, orders.order_id, orders.order_date FROM customers INNER JOIN orders ON customers.id = orders.customer_id;
+/*Full join in mariaDB using union between left and right join*/
+SELECT customers.id, customers.name, orders.order_id, orders.order_date FROM customers LEFT JOIN orders ON customers.id = orders.customer_id
+UNION
+SELECT customers.id, customers.name, orders.order_id, orders.order_date FROM customers RIGHT JOIN orders ON customers.id = orders.customer_id;
