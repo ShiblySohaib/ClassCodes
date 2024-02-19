2c865b475ee43fb168a21f1094512afe5286b6bd
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2024-02-19 15:08:23 +0600

    Create inheritance.dart

---
+++ CSE 335 [Mobile dev]/inheritance.dart
@@ -0,0 +1,35 @@
+import 'package:dart_class/dart_class.dart' as dart_class;
+
+class Person{
+  String? name;
+  int? age;
+}
+
+class Doctor extends Person{
+  List<String>? listofDegrees;
+  String? hospitalName;
+  void display(){
+    print("Name is: $name");
+    print("Age is: $age");
+    print("Degrees are: $listofDegrees");
+    print("Hospital name: $hospitalName");
+  }
+}
+
+class specialist extends Doctor{
+  String? specialization;
+  void display(){
+    super.display();
+    print(specialization);
+  }
+}
+
+void main(){
+  specialist sp = specialist();
+  sp.name = 'ABC';
+  sp.age = 45;
+  sp.listofDegrees = ['MBBS', 'FCPS'];
+  sp.hospitalName = 'Apollo';
+  sp.specialization = 'Cardiologist';
+  sp.display();
+}
