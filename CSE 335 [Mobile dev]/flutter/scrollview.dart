747c0908fef1b8722016912ceb22f692ce9d32ca
Mohammad Sohaib Islam Shibly <86130609+ShiblySohaib@users.noreply.github.com>
2024-05-06 14:12:48 +0600

    Create scrollview.dart

---
+++ CSE 335 [Mobile dev]/flutter/scrollview.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+         appBar: AppBar(
+          title: Text('Scroll_View'),
+          backgroundColor: Colors.blue,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children:[
+              Container(
+                height: 200,
+                color: Colors.red,
+                margin: EdgeInsets.only(bottom:10),
+              ),
+              Container(
+                height: 200,
+                color: Colors.yellow,
+                margin: EdgeInsets.only(bottom:10),
+              ),
+              Container(
+                height: 200,
+                color: Colors.grey,
+                margin: EdgeInsets.only(bottom:10),
+              ),
+              Container(
+                height: 200,
+                color: Colors.cyan,
+                margin: EdgeInsets.only(bottom:10),
+              )
+            ]
+          )
+        )
+      ),
+    );
+  }
+}
